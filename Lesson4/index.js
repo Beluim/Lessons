6e443496c80f90f2144a006a56d0f3ba97b1c13d
Beluim <belui.m09.cvk@gmail.com>
2026-01-06 00:16:19 +0100

    init

---
+++ Lesson4/index.js
@@ -0,0 +1,59 @@
+/*
+const user = {
+    name: "Alex",
+    sayHiStandart: function() {
+        setTimeout(function() {
+            console.log(this.name);
+        }, 1000);
+    },
+
+    sayHiArrow: function() {
+        setTimeout(() => {
+            console.log(this.name);
+        }, 1000);
+    }
+};
+
+user.sayHiStandart();
+user.sayHiArrow();
+*/
+
+/*
+const formatSize = (bytes) => {
+    if (bytes < 1024) {
+        return `${bytes} B`;
+    }
+
+    const mbytes = (bytes / 1024 / 1024).toFixed(2);
+
+    return `${mbytes} MB`
+};
+
+const filesSize = [500, 5e6, 5e5];
+
+for (const fileSize of filesSize) {
+    console.log(formatSize(fileSize));
+}
+*/
+
+/* const usdToRub = (usd) => {
+    if (usd > 300) {
+        return usd * 85;
+    } else if (usd > 1000) {
+        return usd * 90;
+    }
+
+    return usd * 80;
+};
+
+console.log(usdToRub(500)); */
+
+let checkAge = (age) => {
+    if (age >= 18) {
+        return `You are adult`;
+    } else {
+        return `You are minor`;
+    }
+};
+
+console.log(checkAge(22));
